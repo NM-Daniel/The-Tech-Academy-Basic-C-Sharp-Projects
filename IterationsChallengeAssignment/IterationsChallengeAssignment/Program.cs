1972b7cec8b8e53bef4f5280f4b7799ac2e15aac
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-07-08 18:14:55 -0700

    update

--- IterationsChallengeAssignment/IterationsChallengeAssignment/Program.cs
+++ IterationsChallengeAssignment/IterationsChallengeAssignment/Program.cs
@@ -11,7 +11,7 @@ namespace IterationsChallengeAssignment
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Goodbye, World!");
             Console.ReadLine();
 
         }
