8da9e6d264049105a60cb2aad4fcefd4992f6c19
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-07-30 13:04:50 -0700

    CallingMethodsSubmission step 276

---
+++ CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Maths.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    //Class is made public so it can be used anywhere
+    public class Maths
+    {
+        //the static modifier allows the Method(s) to be called in Program.cs without creating an object of the class Maths
+        public static int Square (int num) //This method is called "Square" and it takes in an int called "num" and returns an int
+        {
+            return num * num;
+        }
+        public static int Cube (int num) //This method is called "Cube" and it takes in an int called "num" and returns an int
+        {
+            return num * num * num;
+        }
+        public static int AddFifty (int num) //This method is called "AddFifty" and it takes in an int called "num" and returns an int
+        {
+            return num + 50;
+        }
+    }
+}
---
+++ CallingMethodsSubmissionAssignment/CallingMethodsSubmissionAssignment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number to see some math operations.");
+            //a string input from the user is converted to an int
+            int userInput = Convert.ToInt32(Console.ReadLine());
+
+            //(an instance of Maths would have needed to have been created below - if the methods called were not static in Maths.cs)
+            //Maths math = new Maths();
+
+            //Three new variables are created. Each one calls a method found in class Maths
+            int operation1 = Maths.Square(userInput); //(Would then be math.Square(userInput); - if methods were not static)
+            int operation2 = Maths.Cube(userInput);
+            int operation3 = Maths.AddFifty(userInput);
+
+            //The results are displayed for the user
+            Console.WriteLine(userInput + " squared is " + operation1);
+            Console.WriteLine(userInput + " cubed is " + operation2);
+            Console.WriteLine(userInput + " plus 50 is " + operation3);
+            
+            Console.ReadLine();
+        }
+    }
+}
