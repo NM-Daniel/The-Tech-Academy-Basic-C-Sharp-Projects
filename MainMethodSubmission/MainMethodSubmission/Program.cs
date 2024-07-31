db962b27f7e231dcf27187d0227d45393408ef57
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-07-30 19:21:38 -0700

    MainMethodSubmission step.280

---
+++ MainMethodSubmission/MainMethodSubmission/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            mathOps operation = new mathOps(); //Instantiating the class mathOps as a variable called operation
+            
+            int inputOne = 2;
+            Console.WriteLine(operation.NumAlt(inputOne)); //Calls the first NumAlt Method
+            Console.ReadLine();
+
+            decimal inputTwo = 15.234m;
+            Console.WriteLine(operation.NumAlt(inputTwo)); //Calls the decimal NumAlt Method
+            Console.ReadLine();
+
+            string inputThree = "10";
+            Console.WriteLine(operation.NumAlt(inputThree)); //Calls the string NumAlt Method
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ MainMethodSubmission/MainMethodSubmission/mathOps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodSubmission
+{
+    public class mathOps
+    {
+        public int NumAlt (int num)
+        {
+            return num + 2;
+        }
+           
+        public int NumAlt (decimal num) //Method takes in a decimal data type
+        {
+            decimal answer = num / 3m; //Creates a decimal variable and operates on the num input
+            return Convert.ToInt32(answer); //Converts the answer to an int and returns it
+
+        }
+
+        public int NumAlt (string phrase) //Method takes in a string
+        {
+            int intPhrase = Convert.ToInt32(phrase); //Converts the string to an int named intPhrase
+            return intPhrase - 1; //subtracts 1 from the int and returns it
+        }
+    }
+}
