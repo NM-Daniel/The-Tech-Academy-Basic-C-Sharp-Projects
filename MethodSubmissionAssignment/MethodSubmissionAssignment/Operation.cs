d10373616f25c1d46b730be583f39c175bb8385f
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-08-01 21:01:11 -0700

    MethodSubmissionAssignment step 281

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Operation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    public class Operation
+    {
+        public double Power(double num1, double num2 = 2) //a Method called Power that takes in one or two doubles and returns a double after an operation
+        {
+            double result = Math.Pow(num1, num2); //num1 is given the power of num2
+            return result;
+        }
+
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, please enter a number (Required):");
+            double inputOne = Double.Parse(Console.ReadLine()); //The user input is casted to a double data typed and assigned to a variable called inputOne
+
+            Operation Op = new Operation(); //An instance of the Operation class is created.
+
+            Console.WriteLine("Please enter a second number to perform an operation (Optional. Press enter to skip):");
+
+            //A try block is used first used to work with two user inputs
+            try
+            {
+                double inputTwo = Double.Parse(Console.ReadLine());
+                Console.WriteLine(inputOne + "^" + inputTwo + " is " + Op.Power(inputOne, inputTwo));
+            }
+            //If no input is entered the Format exception error will be handled and we will call the Method without the second input
+            catch (FormatException ex)
+            {
+                Console.WriteLine(inputOne + " squared is " + Op.Power(inputOne));
+            }
+            Console.ReadLine();
+        }
+    }
+}
