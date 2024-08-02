0965810b7f810386ca537586fd3a55577b252866
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-08-02 11:54:47 -0700

    MethodClass step 282

---
+++ MethodClass/MethodClass/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //the class voidMethod is instantiated.
+            voidMethod voidMethod = new voidMethod();
+
+            //MyMethod, created in voidMethod class, is passed two int's
+            voidMethod.MyMethod(1000, 500);
+            Console.ReadLine();
+
+            //Different int's are created and assigned to variables
+            int num1 = 300;
+            int num2 = 450;
+
+            //MyMethod is given two int's but it is passed in as a variable name.
+            voidMethod.MyMethod(num1, num2);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ MethodClass/MethodClass/voidMethod.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClass
+{
+    class voidMethod
+    {
+        //Creating a void method that takes in two integers, called num1 and num2
+        public void MyMethod (int num1, int num2)
+        {
+            //A mathematical operation is run on num1
+            int altered = num1 / 100;
+            //num2 will be the integer that is actually displayed to the screen
+            Console.WriteLine(num2);
+            
+        }
+    }
+}
