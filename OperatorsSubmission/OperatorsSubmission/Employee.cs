9717d3b0b710471f0ffa23f245e408c28e075058
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-08-04 14:05:11 -0700

    OperatorsSubmission step 315

---
+++ OperatorsSubmission/OperatorsSubmission/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    public class Employee
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int Id { get; set; }
+
+        //Comparison operators are overloaded so they compare employee objects based on their Id property
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.Id == employee2.Id;
+        }
+        //Must overloaded the pair
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+            return employee1.Id != employee2.Id;
+        }
+
+    }
+}
---
+++ OperatorsSubmission/OperatorsSubmission/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OperatorsSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Two instances of Employee objects have been created and assigned values for their properties.
+            Employee employee1 = new Employee() { Id = 0, firstName = "Bob", lastName = "Vance" };
+            Employee employee2 = new Employee() { Id = 1, firstName = "Michael", lastName = "Scott" };
+
+            Console.WriteLine("Are Bob and Michael's Id numbers the same?");
+
+            //Using the overloaded comparison operators to compare employee1.Id with employee2.Id
+            bool same = employee1 == employee2;
+            Console.WriteLine(same);
+
+            Console.ReadLine();
+
+        }
+    }
+}
