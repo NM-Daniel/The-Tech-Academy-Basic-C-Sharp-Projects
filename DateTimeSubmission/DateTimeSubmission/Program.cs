41f8e2600633d72c79cedbc4e9d5cc33ec8ca61b
NM-Daniel <165437058+NM-Daniel@users.noreply.github.com>
2024-08-16 16:55:55 -0700

    DateTimeSubmission step 371

---
+++ DateTimeSubmission/DateTimeSubmission/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTimeSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Prints the exact date and time it is currently
+            Console.WriteLine("It is currently:" + DateTime.Now);
+            Console.WriteLine("Enter a number");
+
+            //Takes an input from the user
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            //Creates a new DateTime variable that takes the time now and uses the AddHours() Method to add the users input to the hours
+            DateTime futureTime = DateTime.Now.AddHours(userNum);
+            //Prints the futureTime
+            Console.WriteLine("In {0} hours it will be: {1}", userNum, futureTime);
+            
+            Console.ReadLine();
+        }
+    }
+}
